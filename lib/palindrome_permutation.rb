d7f1d5da642efe69bff0609077d940d76278f1e6
Hannah <hannahjoo24@gmail.com>
2020-03-23 21:34:57 -0700

    Updated palindrome_permutation.rb

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,19 @@
+# Time complexity: O(n) => Because it is not a nested loop and it loops through depending on the input size. It changes from O(2n) to O(n) after dropping a constant, 2.
+
+# Space complexity: O(n) => Because one hash was created. 
 
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
-end
+  # edge case
+  return true if string.empty?
+
+  hash = {} 
+
+  string.length.times do |i| 
+    hash[string[i]] ? hash[string[i]] += 1 : hash[string[i]] = 1 
+  end 
+
+
+  p hash
+
+  hash.count { |letter, count| count % 2 != 0 } > 1 ? false : true 
+end 
